dee42220be9faa199844e5671ed1702da7b12cc7
julianolandim <juliano@ibg.ind.br>
2020-06-13 16:56:46 -0300

    Projeto, previsão do tempo, curso de React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API = "https://api.hgbrasil.com/weather?woeid=457233&format=json-cors&locate=pt";
+
+class App extends React.Component {
+
+    state = {
+        cidade : "",
+        forecast: []
+    }
+
+
+    componentDidMount() {
+        axios.get(API)
+            .then( ({data})  => {
+                console.log(data)
+                this.setState({
+                    cidade : data.results.city,
+                    forecast : data.results.forecast
+                })
+
+            })
+    }
+
+    render() {
+        let {cidade} = this.state;
+        return (
+            <div className="container">
+                <h3>{cidade}</h3>
+                <table  className="striped centered">
+                    <thead>
+                    <tr>
+                        <th>Dia</th>
+                        <th>Min.</th>
+                        <th>Max.</th>
+                        <th>Previsão.</th>
+                        <th>Imagem.</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        this.state.forecast.map( (previsao,index) => {
+                            return (
+                                <tr key={index} >
+                                    <td>{previsao.weekday}</td>
+                                    <td>{previsao.min}</td>
+                                    <td>{previsao.max}</td>
+                                    <td>{previsao.description}</td>
+                                    <td> <img src={`/weather-icons/${previsao.condition}.svg`} width="20%"
+                                              alt={previsao.description} /></td>
+                                </tr>
+                            )
+                        })
+                    }
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+
 }
 
 export default App;
